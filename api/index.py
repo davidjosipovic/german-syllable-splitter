25d5f27a9046ecb9418ba4529afc8a524a2ebd88
David Josipović <davidjosipovic19@gmail.com>
2024-05-06 11:14:08 +0200

    web functionality

--- api/index.py
+++ api/index.py
@@ -1,71 +1,211 @@
-from flask import Flask
+from flask import Flask, request
+import re
+from split_words import Splitter
+from spylls.hunspell import Dictionary
+import json
+
 app = Flask(__name__)
+splitter = Splitter()
+dictionary = Dictionary.from_files('dictionary/de_DE')
 
-def separate_prefixes(word):
-    prefixes = ["an", "ab", "auf", "aus", "dis", "ein", "fehl", "her", "hin", "haupt", "in", "dar", "durch",
-                "los", "mit", "nach","ge", "von", "vor", "weg", "um", "un", "ur", "ent", "er", "ver", "zer", "miss",
-                "miß", "niss", "niß", "ex", "non", "super", "trans", "kon", "hoch", "stink", "stock", "tief",
-                "tod", "erz", "unter", "über", "hinter", "wider", "wieder", "weiter", "zurück", "zurecht",
-                "zusammen", "hyper", "inter"]
 
-    for prefix in prefixes:
-        if word.startswith(prefix) and word[len(prefix)] in 'bcdfghjklmnpqrstvwxyzß':
-            return [prefix, word[len(prefix):]]
+@app.route("/api/python", methods=["POST"])
+def syllableSplitter():
+    request_data = request.get_json()
+    input=request_data['input']
 
-    return ["",word]
+    def is_german(word):
+        return dictionary.lookup(word)
 
-def separate_word(word):
-    vowels = 'aeiouäöü'
-    consonants = 'bcdfghjklmnpqrstvwxyzß'
-    
-    # Rastavljanje tsch
-    if 'tsch' in word:
-        index = word.index('tsch')
-        if index == 0:
-            return 'tsch ' + word[4:]
-        elif index == len(word) - 4:
-            return word[:index] + ' tsch'
-        else:
-            if word[index-1] in vowels:
-                return word[:index] + 't sch' + word[index+4:]
+    def split_and_check_german_compounds(word):
+        compounds = []
+
+        splits = splitter.split_compound(word)
+
+        if not splits:
+            return []
+
+        for i in range(min(len(splits), 3)):
+            first_part = splits[i][1]
+            second_part = splits[i][2]
+
+            if first_part == second_part:
+                return [first_part]
+
+            if is_german(first_part) and is_german(second_part):
+                first_split = split_and_check_german_compounds(first_part)
+                second_split = split_and_check_german_compounds(second_part)
+
+                if first_split and second_split:
+                    compounds.extend(first_split)
+                    compounds.extend(second_split)
+                else:
+                    compounds.append(first_part)
+                    compounds.append(second_part)
+                break
+
+        if compounds == []:
+            compounds = [word]
+
+        return compounds
+
+    def split_sentence_and_check_german_compounds(sentence):
+        words = re.findall(r"[\w']+|[.,!?;]", sentence)
+        final_splits = []
+        for word in words:
+            if word.isalnum():
+                final_split = split_and_check_german_compounds(word)
+                final_splits.extend(final_split)
             else:
-                return word[:index] + ' tsch ' + word[index+4:]
+                final_splits.append(word)
+        return final_splits
 
-    # Rastavljanje VCCV -> VC-CV
-    for i in range(len(word) - 3):
-        if word[i] in vowels and word[i+1] in consonants and word[i+2] in consonants and word[i+3] in vowels:
-            return word[:i+2] + ' ' + word[i+2:]
+    def separate_prefix(word):
+        prefixes = ["an", "ab", "auf", "aus", "dis", "ein", "fehl", "her", "hin", "haupt", "in", "dar", "durch",
+                    "los", "mit", "nach","ge", "von", "vor", "weg", "um", "un", "ur", "ent", "er", "ver", "zer", "miss",
+                    "miß", "niss", "niß", "ex", "non", "super", "trans", "kon", "hoch", "stink", "stock", "tief",
+                    "tod", "erz", "unter", "über", "hinter", "wider", "wieder", "weiter", "zurück", "zurecht",
+                    "zusammen", "hyper", "inter"]
 
-    # Rastavljanje VCCCV -> VCC-CV
-    for i in range(len(word) - 4):
-        if word[i] in vowels and word[i+1] in consonants and word[i+2] in consonants and word[i+3] in consonants and word[i+4] in vowels:
-            return word[:i+3] + ' ' + word[i+3:]
+        for prefix in prefixes:
+            if word.startswith(prefix) :
+                return [prefix, word[len(prefix):]]
 
-    # Rastavljanje VCV -> V-CV
-    for i in range(len(word) - 2):
-        if word[i] in vowels and word[i+1] in consonants and word[i+2] in vowels:
-            return word[:i+1] + ' ' + word[i+1:]
+        return ["", word]
 
-    return word
+    def split_word_into_syllables(word):
+        vowels = ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü', 'ei', 'ai', 'ey', 'ay', 'eu', 'äu', 'ie', 'au', 'aa', 'ee', 'oo']
+        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'ß', 'sch', 'ch', 'ck', 'qu', 'ph']
+        new_word = []
+        
+        i = 0
+        while i < len(word):
+            char = word[i]
+            char_lower = char.lower()
 
-
-# Test
-word = "abgefahren"
-prefix, suffix = separate_prefixes(word)
-prefix2, suffix = separate_prefixes(suffix)
-
-separated_suffix = separate_word(suffix)
-if(prefix!="" and prefix2!=""):
-    separated_word = prefix + " " + prefix2 + " " + separated_suffix
-elif(prefix!="" and prefix2==""):
-    separated_word = prefix + " " + separated_suffix
-else:
-    separated_word = separated_suffix
+            if (char_lower == 't' and i < len(word) - 3 and word[i:i+4] == 'tsch' and i==0) or (char_lower == 'h' and i > 0 and word[i-1:i+3] == 'tsch' and i==0) :
+                new_word.append('tsch')
+                i += 4
+                continue
 
 
-print(f"The separated word is: {separated_word}")
+            if char_lower == 's' and i < len(word) - 2 and word[i+1:i+3] == 'ch':
+                new_word.append('sch')
+                i += 3
+                continue
+
+            sequence = char_lower
+            j = i + 1
+            while j < len(word) and sequence in vowels + consonants and sequence + word[j].lower() in vowels + consonants:
+                sequence += word[j].lower()
+                j += 1
+
+            new_word.append(sequence)
+            i = j
+        
+
+        
+        for i in range(len(new_word) - 3):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2] in consonants and new_word[i+3] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2] in consonants and new_word[i+3] in consonants and new_word[i+3] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+
+        for i in range(len(new_word) - 2):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2] in vowels:
+                new_word = new_word[:i+1] + ['-'] + new_word[i+1:]
+                continue
+
+        for i in range(len(new_word) - 3):
+            if new_word[i] in vowels and new_word[i+1]=="s" and new_word[i+2]=="t" and new_word[i+3] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2]=="s" and new_word[i+3]=="t" and new_word[i+4] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1]=="s" and new_word[i+2]=="t" and new_word[i+3] in consonants and new_word[i+4] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+        
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1]=="x" and new_word[i+2]=="t" and new_word[i+3] in consonants and new_word[i+4] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+
+        for i in range(len(new_word) - 3):
+            if new_word[i] in vowels and new_word[i+1]=="t" and new_word[i+2]=="z" and new_word[i+3] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+        
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2]=="t" and new_word[i+3]=="z" and new_word[i+4] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1]=="t" and new_word[i+2]=="z" and new_word[i+3] in consonants and new_word[i+4] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+        
+        for i in range(len(new_word) - 3):
+            if new_word[i] in vowels and new_word[i+1]=="p" and new_word[i+2]=="f" and new_word[i+3] in vowels:
+                new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2]=="p" and new_word[i+3]=="f" and new_word[i+4] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+
+        for i in range(len(new_word) - 4):
+            if new_word[i] in vowels and new_word[i+1]=="p" and new_word[i+2]=="f" and new_word[i+3] in consonants and new_word[i+4] in vowels:
+                new_word = new_word[:i+3] + ['-'] + new_word[i+3:]
+                continue
+
+        new_word_string=""
+        for el in new_word:
+            new_word_string+=el
+        return new_word_string.replace("-"," ")
+
+    def finish_word(word):
+        prefix, suffix = separate_prefix(word)
+        prefix2, suffix = separate_prefix(suffix)
+
+        prefix=split_word_into_syllables(prefix)
+        prefix2=split_word_into_syllables(prefix2)
+        separated_suffix = split_word_into_syllables(suffix)
+
+        if prefix != "" and prefix2 != "":
+            separated_word = prefix + " " + prefix2 + " " + separated_suffix
+        elif prefix != "" and prefix2 == "":
+            separated_word = prefix + " " + separated_suffix
+        else:
+            separated_word = separated_suffix
+
+        return separated_word
+
+    # Define the sentence
+    sentence = "Diät Knie Knie Auto Seeufer Katze Tatze Pfütze putzen platzen Bürste Kiste Hamster Fenster hinstellen darstellen erstarren plötzlich Postauto Kratzbaum boxen heben rodeln Schifffahrt Mussspiel wichtigsten besuchen gewinnen vergessen abangeln Kreuzotter poetisch Nationen aber über Kreuzklemme Foxtrott witzlos witzig wegschmeißen Bettüberzug wirtschaft Beziehungsknatsch Gletscher Wurstscheibe Borretschgewächs Bodden Handball Neubau Stalltür Autobahnanschlussstelle"
+    sentence=input
+    # Call the function to split and check if the parts are German
+    final_splits = split_sentence_and_check_german_compounds(sentence)
+
+    # Apply syllable splitting to every word in the sentence
+    final_splits_syllable = [finish_word(word.lower()) for word in final_splits]
+    final_splits_syllable=" ".join(final_splits_syllable)
+    final_splits_syllable=json.dumps(final_splits_syllable)
+
+    # Join the splits into a single string with spaces
+    print(final_splits_syllable)
 
 
-@app.route("/api/python")
-def hello_world():
-    return separated_word
+
+    return final_splits_syllable
--- app/page.tsx
+++ app/page.tsx
@@ -1,116 +1,69 @@
-import Image from 'next/image'
-import Link from 'next/link'
+'use client'
+import { useState } from "react";
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [outputValue, setOutputValue] = useState<string>("");
+
+  // Function to handle Enter key press
+  const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      try {
+        // Call your API here with the input value
+        const response = await fetch(`/api/python`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ input: inputValue })
+        });
+
+        if (response.ok) {
+          // If the response is successful, do something with the response
+          const data = await response.json();
+          console.log("API response:", data);
+          setOutputValue(data)
+        } else {
+          // If the response is not successful, handle the error
+          console.error("API request failed:", response.status, response.statusText);
+        }
+      } catch (error) {
+        // If an error occurs during the fetch call, handle the error
+        console.error("Error:", error);
+      }
+    }
+  };
+
+  // Function to handle button click to set input value
+  const handleSetInputValue = () => {
+    setInputValue(`Diät Knie Knie Auto Seeufer Katze Tatze Pfütze putzen platzen Bürste Kiste Hamster Fenster hinstellen darstellen erstarren plötzlich Postauto Kratzbaum boxen heben rodeln Schifffahrt Mussspiel wichtigsten besuchen gewinnen vergessen abangeln Kreuzotter poetisch Nationen aber über Kreuzklemme Foxtrott witzlos witzig wegschmeißen Bettüberzug wirtschaft Beziehungsknatsch Gletscher Wurstscheibe Borretschgewächs Bodden Handball Neubau Stalltür Autobahnanschlussstelle`);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
-          </Link>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex items-center justify-center h-screen">
+      <div className="w-1/3">
+        {/* Input field */}
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyPress={handleKeyPress} // Add event handler for Enter key press
+          className="w-full px-4 py-2 text-lg border border-gray-300 rounded-lg shadow-md focus:outline-none focus:border-blue-500"
+          placeholder="Enter something..."
         />
-      </div>
+        <p className="mt-0 text-sm">Press ENTER to split</p>
+        {/* Output element */}
+        <h2 className="mt-4 text-xl underline">Output:</h2>
+        <p className="mt-0 text-xl bg-gray-100 rounded-md">{outputValue}</p>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Button to set input value */}
+        <button
+          onClick={handleSetInputValue}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Set Test Input
+        </button>
       </div>
     </main>
-  )
+  );
 }
