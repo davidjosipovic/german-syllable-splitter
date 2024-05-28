3759d6c79dcbab2a6c0c53b7d596de91f707b23b
David Josipović <davidjosipovic19@gmail.com>
2024-05-28 18:35:36 +0200

    Capitalization

--- api/index.py
+++ api/index.py
@@ -16,22 +16,20 @@ def syllableSplitter():
     input=request_data['input']
 
     def is_german(word):
-        
-        if word=="Ans" or word=="An" or word=="und":
+        true_words=["Sonn","Lich"]
+        false_words=["Ans","An","Und","Freund","Ist","Ich","Etsch"]
+
+        if word in false_words:
             return False
-        if word=="Ich" or word=="ich":
-            return False
-        if word=="Etsch":
-            return False
-        if string.capwords(word)=="Sonn":
+        if word in true_words:
             return True
 
         return dictionary.lookup(string.capwords(word))
 
     def split_and_check_german_compounds(word, depth=2):
-        bad_words=["Nationen,und"]
+        not_compound_word=["nationen","und","brandenburger","versprochen"]
         compounds = []
-        if word in bad_words:
+        if word.lower() in not_compound_word:
             return [word]
         splits = splitter.split_compound(word)
     
@@ -60,6 +58,10 @@ def syllableSplitter():
         if compounds == []:
             compounds = [word]
         
+        compounds=[c.lower() for c in compounds]
+        if word.istitle():
+            compounds[0]=compounds[0].title()
+
         return compounds
 
 
@@ -73,18 +75,33 @@ def syllableSplitter():
                 final_splits.extend(final_split)
             else:
                 final_splits.append(word)
-        return final_splits
+        return final_splits,words
 
     def separate_prefix(word):
         prefixes = ["an", "ab", "auf", "aus", "dis", "ein", "fehl", "her", "hin", "haupt", "in", "dar", "durch",
-                    "los", "mit", "nach","ge","leb", "von", "vor", "weg", "um", "un", "ur", "ent", "er", "ver", "zer", "miss",
+                    "los", "mit", "nach","ge", "von", "vor", "weg", "um", "un", "ur", "ent", "er", "ver", "zer", "miss",
                     "miß", "niss", "niß", "ex", "non", "super", "trans", "kon", "hoch", "stink", "stock", "tief",
                     "tod", "erz", "unter", "über", "hinter", "wider", "wieder", "weiter", "zurück", "zurecht",
-                    "zusammen", "hyper", "inter"]
+                    "zusammen", "hyper", "inter","phy","sys","ruhm","rühm","alex","be"]
+        
+        excluded_suffixes = ["ssen", "ts", "er"]
+        excluded_words = ["und", "geben", "wege", "ins","besten"]
+        excluded_prefixes = ["eine", "eini","berlin","erst"]
+        special_case = "gereist"
+
 
         for prefix in prefixes:
-            if word.startswith(prefix) and word[len(prefix):]!="ssen" and word[len(prefix):]!="ts" and word[len(prefix):]!="er" and word!="und" and word.startswith("eine")==False and word!="gerne" :
-                return [prefix, word[len(prefix):]]
+            word_suffix = word[len(prefix):]
+            
+            if (
+                word.startswith(prefix) and 
+                len(word_suffix)>1 and
+                word_suffix not in excluded_suffixes and 
+                word not in excluded_words and 
+                not any(word.startswith(ex_prefix) for ex_prefix in excluded_prefixes) and 
+                (not word.startswith("ger") or word == special_case)
+            ):
+                return [prefix, word_suffix]
 
         return ["", word]
 
@@ -95,6 +112,11 @@ def syllableSplitter():
         consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'ß', 'sch', 'ch', 'ck', 'qu', 'ph']
         new_word = []
         forbidden=['sk','br']
+        no_split_word=["tipps","kommt","fällt"]
+    
+
+        if word in no_split_word:
+            return word
         
         i = 0
         while i < len(word):
@@ -121,6 +143,15 @@ def syllableSplitter():
             new_word.append(sequence)
             i = j
         
+        # VthV->V-thV
+        i=0
+        while i<(len(new_word)- 3):
+            if new_word[i] in vowels and new_word[i+1]=="t" and new_word[i+2]=="h" and new_word[i+3] in vowels:
+                new_word = new_word[:i+1] + ['-'] + new_word[i+1:]
+                continue
+            i+=1
+
+        
         # VCCV->VC-CV
         i=0
         while i<(len(new_word)- 3):
@@ -142,10 +173,12 @@ def syllableSplitter():
         # VCV->V-CV
         i=0
         while i<(len(new_word)- 2):
-            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2] in vowels and (i!=0 or new_word[i] in vowelsdouble):
+            if new_word[i] in vowels and new_word[i+1] in consonants and new_word[i+2] in vowels and (i!=0 or new_word[i] in vowelsdouble) and "".join(new_word[i:i+3])!="ice":
                 new_word = new_word[:i+1] + ['-'] + new_word[i+1:]
                 continue
             i+=1
+
+       
 
         # VstV->Vs-tV
         i=0
@@ -247,21 +280,25 @@ def syllableSplitter():
         # 2 ista suglasnika u sredini riječi se dijele
         i=0
         while i<(len(new_word)- 3):
-            if new_word[i+1] in consonants and new_word[i+2] in consonants and new_word[i+1]== new_word[i+2] :
+            if new_word[i+1] in consonants and new_word[i+2] in consonants and new_word[i+1]== new_word[i+2]:
                 new_word = new_word[:i+2] + ['-'] + new_word[i+2:]
                 continue
             i+=1
 
-
+        
+        
         new_word_string=""
         for el in new_word:
             new_word_string+=el
-        return new_word_string.replace("-"," ")
+        new_word_string=new_word_string.replace("-"," ")
+        if word.istitle()==True:
+            new_word_string=new_word_string[0].upper()+new_word_string[1:]
+        print(new_word_string)
+        return new_word_string
 
     def finish_word(word):
         prefix, suffix = separate_prefix(word)
         prefix2, suffix = separate_prefix(suffix)
-
         prefix=split_word_into_syllables(prefix)
         prefix2=split_word_into_syllables(prefix2)
         separated_suffix = split_word_into_syllables(suffix)
@@ -278,16 +315,20 @@ def syllableSplitter():
     # Define the sentence
 
     # Call the function to split and check if the parts are German
-    final_splits = split_sentence_and_check_german_compounds(input)
+    final_splits,words = split_sentence_and_check_german_compounds(input)
+    print("Normal words:",words)
     # Apply syllable splitting to every word in the sentence
-    final_splits_syllable = [finish_word(word.lower()) for word in final_splits]
+    final_splits_syllable = [finish_word(word) for word in final_splits]
+    print(final_splits)
+
+
+
     final_splits_syllable=" ".join(final_splits_syllable)
     final_splits_syllable=re.sub(r'\s{2,}', ' ', final_splits_syllable)
     final_splits_syllable = re.sub(r'\s+([.,!?;:])', r'\1', final_splits_syllable)
     # Join the splits into a single string with spaces
     final_splits_syllable=json.dumps(final_splits_syllable)
     # Join the splits into a single string with spaces
-    print(final_splits_syllable)
 
 
     return final_splits_syllable
